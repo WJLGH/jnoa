5bcc99b83a487e86982ddff963bc658bdaa38d45
wjl <1179315470@qq.com>
2019-02-03 23:57:00 +0800

    2019-02-03 完成了按年月日，按账户汇总信息的后台接口

--- src/main/java/com/thinkgem/jeesite/modules/finance/dao/FinRecordDao.java
+++ src/main/java/com/thinkgem/jeesite/modules/finance/dao/FinRecordDao.java
@@ -5,7 +5,10 @@ package com.thinkgem.jeesite.modules.finance.dao;
 
 import com.thinkgem.jeesite.common.persistence.CrudDao;
 import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
+import com.thinkgem.jeesite.modules.account.entity.FinAccount;
 import com.thinkgem.jeesite.modules.finance.entity.FinRecord;
+
+import java.util.List;
 
 /**
  * 明细记录DAO接口
@@ -14,5 +17,13 @@ import com.thinkgem.jeesite.modules.finance.entity.FinRecord;
  */
 @MyBatisDao
 public interface FinRecordDao extends CrudDao<FinRecord> {
-	
+	List<FinRecord> findListBeginToEnd(FinRecord finRecord);
+
+    List<FinRecord> findListByAccount(FinAccount finAccount);
+
+    List<FinRecord> findListByBusType(FinRecord finRecord);
+
+    List<FinRecord> getBusTypsList(FinRecord finRecord);
+
+    List<FinRecord> getDateSumList(FinRecord finRecord);
 }
